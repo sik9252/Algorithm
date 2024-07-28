2cecc89303cfa99d649867150f56b7679d8e05c8
JiHyun Lee <64947440+sik9252@users.noreply.github.com>
2024-07-28 19:29:45 +0900

    [Silver I] Title: 카드 합체 놀이, Time: 236 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.py
+++ 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.py
@@ -3,12 +3,10 @@ input=sys.stdin.readline
 
 n, m = map(int, input().split())
 cards = sorted(list(map(int, input().split())))
-total = sum(cards)
 
 for _ in range(m):
     a, b = cards[0], cards[1]
     cards[0], cards[1] = a+b, a+b
     cards = sorted(cards)
-    total = sum(cards)
 
-print(total)
+print(sum(cards))
