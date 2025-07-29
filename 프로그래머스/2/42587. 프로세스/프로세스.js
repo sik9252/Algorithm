ed14319db9339d629e5ee737005f5d47c1dc6e62
JiHyun Lee <64947440+sik9252@users.noreply.github.com>
2025-07-29 11:15:01 +0900

    [level 2] Title: 프로세스, Time: 0.27 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -0,0 +1,22 @@
+function solution(priorities, location) {        
+    let count = 0;
+    let maxPriority = Math.max(...priorities);
+    
+    while (true) {
+        const currentProcess = priorities.shift();
+        
+        if (currentProcess === maxPriority) {
+            count ++;
+            if (location === 0) {
+                return count;
+            }
+            maxPriority = Math.max(...priorities);
+        } else {
+            priorities.push(currentProcess);
+        }
+        
+        location = location === 0 ? priorities.length - 1 : location - 1;
+    }
+    
+    return count;
+}
