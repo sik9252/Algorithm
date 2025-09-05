6a284c6e2772d6a0111a8b099b4bee698f4ff9f7
JiHyun Lee <64947440+sik9252@users.noreply.github.com>
2025-09-05 16:18:17 +0900

    [Silver IV] Title: 괄호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.cc
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int t;
+string s;
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	
+	cin >> t;
+	
+	while (t--) {
+		cin >> s;
+		stack<char> st;
+		
+		for (int i = 0; i < s.length(); i++) {
+			char cur = s[i];
+			
+			if (!st.empty() && st.top() == '(' && cur == ')') {
+				st.pop();
+			} else {
+				st.push(cur);
+			}
+		}
+		
+		if (st.empty()) {
+			cout << "YES" << "\n";
+		} else {
+			cout << "NO" << "\n";
+		}
+	}
+		
+	return 0;
+}
