9b38711c21127f4a00e028db7b49090d659e064f
JiHyun Lee <sik9252@naver.com>
2026-03-05 14:03:49 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 116.58 ms, Memory: 58.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 70.8 MB, 시간: 65.37 ms
+메모리: 58.8 MB, 시간: 116.58 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 28일 22:42:39
+2026년 03월 05일 14:03:47
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,26 +1,14 @@
 function solution(participant, completion) {
-    var answer = ""
-    let map = new Map();
+    var answer = '';
     
-    for (let i=0; i<participant.length; i++) {
-        if (map.has(participant[i])) {
-            map.set(participant[i], map.get(participant[i]) + 1)
-        } else {
-           map.set(participant[i], 1) 
-        }
-    }
+    participant.sort();
+    completion.sort();
     
-    for (let i=0; i<completion.length; i++) {
-        if (map.has(completion[i])) {
-            map.set(completion[i], map.get(completion[i]) - 1);
-        }
-    }
-    
-    for (let [key, value] of map) {
-        if (value !== 0) {
-            answer = key;
+    for (let i = 0; i < completion.length; i++) {
+        if (participant[i] !== completion[i]) {
+            return participant[i];
         }
     }
 
-    return answer;
+    return participant[participant.length - 1];
 }
