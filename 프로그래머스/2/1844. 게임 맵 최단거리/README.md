8813e8826ce3788032bf95df90e52d71f1c24b8d
JiHyun Lee <sik9252@naver.com>
2026-03-06 15:57:47 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 13.29 ms, Memory: 38.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 37.8 MB, 시간: 32.55 ms
+메모리: 38.5 MB, 시간: 13.29 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 02일 18:51:11
+2026년 03월 06일 15:57:46
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -6,33 +6,31 @@ function solution(maps) {
     
     const n = maps.length;
     const m = maps[0].length;
+    const dist = Array.from({length: n}, () => Array(m).fill(0));
+    dist[0][0] = 1;
     
-    const visited = Array.from({length: n}, () => Array(m).fill(false));
-    const queue = [];
+    const queue = [[0, 0]];
+    let head = 0;
     
-    queue.push([0, 0]);
-    visited[0][0] = true;
-    
-    while (queue.length) {
-        const [y, x] = queue.shift();
+    while (head < queue.length) {
+        const [x, y] = queue[head++];
+        
+        if (x === n-1 && y === m-1) {
+            return dist[x][y];
+        }
         
         for (let i = 0; i < 4; i++) {
             const nx = x + dx[i];
             const ny = y + dy[i];
             
-            if (ny >= 0 && ny < n && nx >= 0 && nx < m && maps[ny][nx] === 1 && !visited[ny][nx]) {
-                visited[ny][nx] = true;
-                maps[ny][nx] = maps[y][x] + 1;
-                queue.push([ny, nx]);
-            }
+            if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+            if (maps[nx][ny] !== 1 || dist[nx][ny] !== 0) continue;
+            
+            dist[nx][ny] = dist[x][y] + 1;
+            queue.push([nx, ny]);
+            
         }
     }
     
-    if (maps[n-1][m-1] === 1) {
-        answer = -1;
-    } else {
-        answer = maps[n-1][m-1];
-    }
-
-    return answer;
+    return -1;
 }
