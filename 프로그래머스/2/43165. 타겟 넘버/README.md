3b9acc6e8b061a68d2ceca6707248b43028ed21b
JiHyun Lee <sik9252@naver.com>
2026-03-06 13:55:32 +0900

    [level 2] Title: 타겟 넘버, Time: 13.85 ms, Memory: 35.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 278 MB, 시간: 2284.26 ms
+메모리: 35.5 MB, 시간: 13.85 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 02일 16:16:30
+2026년 03월 06일 13:55:30
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,38 +1,17 @@
-function permutationWithRepetition(arr, r) {
-    const result = [];
+function solution(numbers, target) {
+    var answer = 0;
     
-    function dfs(path) {
-        if (path.length === r) {
-            result.push([...path]);
+    function dfs(sum, idx) {
+        if (idx === numbers.length) {
+            if (sum === target) answer++;
             return;
         }
         
-        for (const item of arr) {
-            path.push(item);
-            dfs(path);
-            path.pop();
-        }
+        dfs(sum + numbers[idx], idx + 1);
+        dfs(sum - numbers[idx], idx + 1);
     }
     
-    dfs([]);
-    return result;
-}
-
-function solution(numbers, target) {
-    var answer = 0;
-    const cases = permutationWithRepetition(["+", "-"], numbers.length);
-    
-    for (let i=0; i < cases.length; i++) {
-        let sum = 0;
-        
-        for (let j=0; j < numbers.length; j++) {
-            sum += Number(cases[i][j] + numbers[j])
-        }
-        
-        if (sum === target) {
-            answer++;
-        }
-    }
+    dfs(0, 0);
     
     return answer;
 }
