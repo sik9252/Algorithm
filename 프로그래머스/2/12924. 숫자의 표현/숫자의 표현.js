16696ac60452aa6b44263c0fed66c826dbf52c9e
JiHyun Lee <sik9252@naver.com>
2026-03-09 21:28:13 +0900

    [level 2] Title: 숫자의 표현, Time: 1.56 ms, Memory: 36.2 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -1,19 +1,18 @@
 function solution(n) {
     var answer = 0;
-    
     let left = 1;
-    let right = 1
+    let right = 1;
     let sum = 1;
     
     while (left <= n) {
-        if (sum < n) {
-            right++;
-            sum += right;
-        } else if (sum > n) {
+        if (sum === n) {
+            answer++;
             sum -= left;
             left++;
+        } else if (sum < n) {
+            right++;
+            sum += right;
         } else {
-            answer++;
             sum -= left;
             left++;
         }
