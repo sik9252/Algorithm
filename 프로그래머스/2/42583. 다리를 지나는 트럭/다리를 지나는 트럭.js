96f482bdd8b1ffb90daa0e2dd1fcff56f7530b0b
JiHyun Lee <sik9252@naver.com>
2026-03-09 11:55:14 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 6.96 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -1,20 +1,25 @@
-function solution(bridge_length, weight, truck_weights) {
-    let answer = 0;
-    let bridge = Array(bridge_length).fill(0);
-  
-    while (bridge.length) {
-        bridge.shift();
-        answer += 1;
-
-        if (truck_weights.length)  {
-            let sum = bridge.reduce((pre, curr) => pre + curr, 0);
-            if (sum + truck_weights[0] <= weight) {
-                bridge.push(truck_weights.shift());
-            } else {
-                bridge.push(0);
-            }
+function solution(bridge_length, weight, truck_weights) {    
+    const bridge = Array.from({length: bridge_length}, () => 0);
+    let currentBridgeWeight = 0;
+    let time = 0;
+    let head = 0;
+    let truckIdx = 0;
+    
+    while (truckIdx < truck_weights.length || currentBridgeWeight > 0) {
+        const out = bridge[head];
+        currentBridgeWeight -= out;
+        bridge[head] = 0;
+        time++;
+        
+        const next = truck_weights[truckIdx];
+        if (currentBridgeWeight + next <= weight) {
+            bridge[head] = next;
+            currentBridgeWeight += next;
+            truckIdx++;
         }
+        
+        head = (head + 1) % bridge_length;
     }
-
-    return answer;
+    
+    return time;
 }
