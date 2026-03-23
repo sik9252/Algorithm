e4c3e296b3fae91acd13527d0cdf8ad91f24c259
JiHyun Lee <sik9252@naver.com>
2026-03-23 10:53:46 +0900

    [level 3] Title: 정수 삼각형, Time: 4.23 ms, Memory: 41.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -0,0 +1,9 @@
+function solution(triangle) {
+  for (let r = triangle.length - 2; r >= 0; r--) {
+    for (let c = 0; c < triangle[r].length; c++) {
+      triangle[r][c] += Math.max(triangle[r + 1][c], triangle[r + 1][c + 1]);
+    }
+  }
+
+  return triangle[0][0];
+}
