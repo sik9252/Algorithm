284f4f717617458e1f7ade0af59f054c50492984
JiHyun Lee <sik9252@naver.com>
2026-03-25 13:33:03 +0900

    [level 2] Title: 피보나치 수, Time: 21.48 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -1,11 +1,12 @@
 function solution(n) {
-    const dp = Array(n+1).fill(0);
+    var answer = 0;
     
+    const dp = Array(n+1).fill(0);
     dp[0] = 0;
     dp[1] = 1;
     
-    for (let i = 2; i <=n; i++) {
-        dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
+    for (let i = 2; i <= n; i++) {
+        dp[i] = (dp[i-2] + dp[i-1]) % 1234567;
     }
     
     return dp[n];
