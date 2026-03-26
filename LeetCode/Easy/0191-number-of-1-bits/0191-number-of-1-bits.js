7bfb379d7c815bd0e6a6ea4048d1feaf00f19c83
JiHyun Lee <sik9252@naver.com>
2026-03-26 19:43:09 +0900

    0 ms (100%) 54.3 MB (69.48%) Easy .js

---
+++ LeetCode/Easy/0191-number-of-1-bits/0191-number-of-1-bits.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var hammingWeight = function(n) {
+    let count = 0;
+
+    while (n !== 0) {
+        n = n & (n - 1);
+        count++;
+    }
+
+    return count;
+};
