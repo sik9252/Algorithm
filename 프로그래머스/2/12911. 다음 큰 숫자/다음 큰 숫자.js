75c65ec4cdc7c0a03fea8f43085668974acc8cde
JiHyun Lee <sik9252@naver.com>
2026-04-22 09:16:17 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.09 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -1,17 +1,12 @@
 function solution(n) {
-    var answer = 0;
-    let standOneCount = countOne(n);
-    
-    for (let i = n + 1; i <= 1000000; i++) {
-       if(standOneCount === countOne(i)) {
-           answer = i;
-           break;
-       }
-    }
-    
-    return answer;
-}
+  const countOne = (num) => num.toString(2).split("1").length - 1;
 
-function countOne(num) {
-    return num.toString(2).split("").reduce((cnt, elem) => cnt + (elem === "1"), 0);
+  const target = countOne(n);
+  let next = n + 1;
+
+  while (countOne(next) !== target) {
+    next++;
+  }
+
+  return next;
 }
